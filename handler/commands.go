82acbb82f6fece0f02cafc9da34c482a6a1d1ef4
nabomhalang <nabomhalangkr@naver.com>
2023-10-08 17:28:26 +0000

    init

--- handler/commands.go
+++ handler/commands.go
@@ -2,7 +2,7 @@ package handler
 
 import (
 	"github.com/bwmarrin/discordgo"
-	"github.com/nabomhalang/halangcordgo/commands/server"
+	Server "github.com/nabomhalang/halangcordgo/commands/server"
 )
 
 type CommandHandler interface {
@@ -11,5 +11,5 @@ type CommandHandler interface {
 }
 
 var CommandHandlers []CommandHandler = []CommandHandler{
-	&server.PingCommandHandler{},
+	&Server.PingCommandHandler{},
 }
